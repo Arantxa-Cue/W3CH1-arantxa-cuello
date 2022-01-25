e92d7d89c9ee2e7bda776c06fbda18e53058ee4e
Arantxa Cuello <arantxa_312@hotmail.com>
2022-01-25 17:49:46 +0100

    component file created

--- web/js/Rey.js
+++ web/js/Rey.js
@@ -3,18 +3,9 @@ import Personaje from "./Personaje.js";
 export default class Rey extends Personaje {
   anyosReinado;
 
-  emoji;
-
-  constructor(nombreRey, familiaRey, edadRey, anyosReinadoRey, emojiRey, card) {
+  constructor(nombreRey, familiaRey, edadRey, anyosReinadoRey) {
     super(nombreRey, familiaRey, edadRey);
     this.anyosReinado = anyosReinadoRey;
-
-    this.emoji = emojiRey;
-    this.emojiRey = document.createElement("i");
-    this.emojiRey.className = "emoji";
-    this.emojiRey.innerHTML = "<i>\uD83D\uDC51</i>";
-
-    card.append(this.emojiRey);
   }
 
   comunicar() {
--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,1 @@
 import personajesGot from "./Personajes.js";
-
-const newEmoji = new Rey(card, "<i>\uD83D\uDC51</i>");
-
-console.log(newEmoji);
