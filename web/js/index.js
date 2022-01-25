1a869e57cd7eb2697a8d5dfdb6e64abf9686d54d
Arantxa Cuello <arantxa_312@hotmail.com>
2022-01-25 19:44:36 +0100

    card component added

--- web/js/index.js
+++ web/js/index.js
@@ -1,12 +1,16 @@
 import ButtonComponent from "../../Components/ButtonComponent.js";
+import CardComponent from "../../Components/CardComponent.js";
 import ImageComponent from "../../Components/ImageComponent.js";
 import personajesGot from "./Personajes.js";
 
 const body = document.querySelector("body");
 const buttonTest = document.querySelector("button");
+const li = document.querySelector("li");
+const cardTest = document.querySelector(".character");
 
 const action = () => {
   console.log("hello");
 };
 const prueba = new ImageComponent(body, "", "señor");
 const button = new ButtonComponent(body, "algo", action());
+const cardTemplate = new CardComponent(cardTest, "div");
